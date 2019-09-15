6c5c10ee6cd74eb0f03dcfcccc20f13cb269d8ac
moT01 <tmondloch01@gmail.com>
2019-09-14 19:04:01 -0500

    fix: rename-files+remove-marker-transition

--- register-service-worker.js
+++ register-service-worker.js
@@ -1,5 +1,5 @@
 if(navigator.serviceWorker) {
-  navigator.serviceWorker.register('/sw.js')
+  navigator.serviceWorker.register('/service-worker.js')
     .then((reg) => console.log('sw registered', reg))
     .catch((err) => console.error('sw not registered', err))
 }
